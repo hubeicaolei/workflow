bec110eebe1c13ee4957b80a8a5b5c71c6c70bd4
caolei <caolei@1jz.me>
2015-04-07 23:00:00 +0800

    added http client component

---
+++ utils/src/main/java/cn/liley/hummer/utils/webservice/WebServiceManager.java
@@ -0,0 +1,15 @@
+package cn.liley.hummer.utils.webservice;
+
+/**
+ * Created by caolei on 2015/4/7.
+ */
+public class WebServiceManager {
+
+    private String host = "localhost";
+    private String port = "3411";
+
+    private String context = "/rest/";
+
+
+
+}
