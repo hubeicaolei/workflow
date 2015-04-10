e04fc4457ae56e836432e093c10532c6411d1f5e
leicaolc <leicaolc@cn.ibm.com>
2015-04-10 17:38:06 +0800

    added nutz.web

---
+++ rest-api/src/main/java/cn/liley/hummer/ws/MainModule.java
@@ -0,0 +1,12 @@
+package cn.liley.hummer.ws;
+
+import cn.liley.hummer.ws.system.SystemModule;
+import org.nutz.mvc.annotation.Modules;
+
+/**
+ * Created by caolei on 2015/4/10.
+ */
+@Modules({ SystemModule.class})
+public class MainModule {
+
+}
---
+++ rest-api/src/main/java/cn/liley/hummer/ws/system/SystemModule.java
@@ -0,0 +1,7 @@
+package cn.liley.hummer.ws.system;
+
+/**
+ * Created by caolei on 2015/4/10.
+ */
+public class SystemModule {
+}
